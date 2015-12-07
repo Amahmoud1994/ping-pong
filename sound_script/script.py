5d48e55ac8508500fe2813b442439bae9c4a74d2
kady <elkadyabdelrahman@gmail.com>
2015-12-07 21:19:22 +0200

    Using serial port to trigger soundeffects on PC

---
+++ sound_script/script.py
@@ -0,0 +1,27 @@
+import serial
+import pyglet
+from time import sleep
+
+ser = serial.Serial('/dev/ttyACM1', 115200, timeout=None)
+
+pyglet.options['audio'] = ('openal', 'silent')
+player = pyglet.media
+
+bat_hit = player.load('effects/Bathit.wav', streaming=False)
+bat_show = player.load('effects/BatShow.wav', streaming=False)
+goal = player.load('effects/Goal.wav', streaming=False)
+round_start = player.load('effects/Start.wav', streaming=False)
+
+while True:
+
+    data = ser.read(1)
+    if data == 'h':
+        bat_hit.play()
+    elif data == 'b':
+        bat_show.play()
+    elif data == 'g':
+        goal.play()
+    elif data == 's':
+        round_start.play()
+
+    sleep(0.001)
